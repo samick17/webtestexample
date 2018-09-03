7c1e3d86d263fe1120434a0be35c6155dffbbbd7
samick17 <boneache@gmail.com>
2018-09-03 22:04:49 +0800

    1. init example for simple-web-automator

---
+++ index.js
@@ -0,0 +1,11 @@
+const simpleWebAutomator = require('simple-web-automator');
+
+//run
+simpleWebAutomator
+.setwd('./testscripts')
+.run('testcase1');
+
+//run step-by-step
+// simpleWebAutomator
+// .setwd('./testscripts')
+// .runcli('testcase1');
---
+++ testscripts/testcase1.js
@@ -0,0 +1,11 @@
+this.data = {
+	
+};
+
+this.steps = [{
+	openApp: []
+},{
+	navigate: ['https://www.google.com']
+},{
+	enterText: ['#lst-ib', 'Stackoverflow\n']
+}];
